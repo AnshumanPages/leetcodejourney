842908e356be447f53aac66fd09ea80eea0862e0
AnshumanPages <anshumanchoudhary212@gmail.com>
2026-08-08 23:10:15 +0530

    Time: 87 ms (44.59%), Space: 56.2 MB (39.96%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int atmost(vector<int>& nums, int k){
+        int l=0,r=0,cnt=0;
+        unordered_map<int ,int> mpp;
+        while(r<nums.size()){
+            mpp[nums[r]]++;
+
+        
+        while(mpp.size()>k){
+            mpp[nums[l]]--;
+            if(mpp[nums[l]]==0){
+                mpp.erase(nums[l]);
+                
+            }
+           l++;
+        }
+         cnt=cnt+(r-l+1);
+            r++;
+            
+        }
+        return cnt;
+        
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+      int  a=atmost(nums,k);
+      int  b=atmost(nums,k-1);
+        return a-b;
+    }
+};
