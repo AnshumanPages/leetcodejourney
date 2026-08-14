687518863fb9acbaed2e358780e14d5eb07db9d8
AnshumanPages <anshumanchoudhary212@gmail.com>
2026-08-14 16:33:53 +0530

    Time: 8 ms (81.94%), Space: 12 MB (23.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -13,8 +13,9 @@ public:
         ListNode*fast=head;
         ListNode*slow=head;
         while(fast!=nullptr && fast->next!=nullptr){
+             slow=slow->next;
             fast=fast->next->next;
-            slow=slow->next;
+           
               if(fast==slow) return true;
            
         }
